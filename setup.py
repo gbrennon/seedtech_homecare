4261020526ecf40ba3d194d9c82793b6f42e136a
gbrennon <glauberbrennon@gmail.com>
2014-05-30 09:59:24 -0300

    Tentando deixar no padroa openshift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SeedTech Homecare Automation',
+      version='1.0',
+      description='OpenShift App',
+      author='Glauber Brennon',
+      author_email='glauberbrennon@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+     )
